a62266080b631b6c46611a1c0f7885a30c40c1eb
mihail-mojsoski <mihailmojsoskivr@outlook.com>
2017-04-02 05:38:48 +0200

    Added the ls command because it was implemented in the original source (it calls the shell to execute dir).

---
+++ Memphis/Commands/Ls.cs
@@ -0,0 +1,18 @@
+﻿namespace Memphis.Commands
+{
+    public class Ls : Command
+    {
+        public override string Id
+        {
+            get
+            {
+                return "ls";
+            }
+        }
+
+        public override void Main(string[] args)
+        {
+            currentShell.Interpret("dir");
+        }
+    }
+}
