cd528b36eb97a4b5dcd052b81b608b94dfabdb39
mihail-mojsoski <mihailmojsoskivr@outlook.com>
2017-04-03 16:03:15 +0200

    Implemented extensions for going up one folder (used in dir ..., string.Up()), lexing the command input (string.Lex(), thanks to @zarlo 's PR #2), and removing the last / first elements in an array (T[].RemoveFirst<T>() and T[]RemoveLast<T>()).

--- Memphis/Commands/Echo.cs
+++ Memphis/Commands/Echo.cs
@@ -1,4 +1,5 @@
 ﻿using Memphis.Shell;
+using Memphis.Extensions;
 
 namespace Memphis.Commands
 {
---
+++ Memphis/Extensions/Lex.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Memphis.Extensions
+{
+    public static partial class Extension
+    {
+        public static string[] Lex(this string s)
+        {
+            bool isinquotes = false;
+            List<string> tokens = new List<string> { "" };
+            foreach (char c in s)
+            {
+                if (c == '"') { isinquotes = !isinquotes; }
+                else if (char.IsWhiteSpace(c) && isinquotes == false) { tokens.Add(""); }
+                else { tokens[tokens.Count - 1] += c; }
+            }
+            return tokens.ToArray();
+        }
+    }
+}
